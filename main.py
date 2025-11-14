964b481f224fc02fc0fbed245b75a8f2800c7f90
botCuaQuan <Quanbocap3@gmail.com>
2025-11-15 03:37:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ async def stop_bot():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
